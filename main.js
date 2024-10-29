a9043ae2cac68344e1d14516cfcd0833d579c647
Jarakei43 <134204371+Jarakei43@users.noreply.github.com>
2024-10-29 18:11:47 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+//N1
+// const str ="Привет, как дела?"
+// console.log(str.split(" "));
+
+// N2
+// const str = "яблоко,банан,груша,апельсин"
+// const arr = str.split(",")
+// console.log(arr);
+
+// N3
+// const arr = ["Привет", "мир", "!"]
+// const str = arr.join(" ")
+// console.log(str);
+
+// N4
+// const word = "h e l l o"
+// const reversed = word.split(' ').reverse().join("")
+// console.log(reversed);
+
+// N5
+// const str = "Каждое слово начинается с буквы";
+// const arr = str.split(" ");
+// const newArr = [];
+// arr.forEach((item) => {
+//   newArr.push(item.charAt(0));
+// });
+// console.log(newArr);
+
+// N6
+// const str = "Привет как дела"
+// const arr = str.split(" ")
+// console.log(arr.join("-"));
+
+// N7
+// const str = "JavaScript - это весело"
+// const arr = str.split(" ")
+// console.log(arr);
+// console.log(arr.length);
+
+// N8
+// const hello = "   Привет   как    дела   "
+// const helloArr = hello.split(" ")
+// console.log(helloArr.join(" ").trim())
+
+// N9
+// const num = [1, 2, 3, 4, 5]
+// console.log(num.join());
+
+//N10
+// const numStr = "1 2 3 4 5";
+// const numArr = numStr.split("");
+// const num = Number(numArr.join());
+// console.log(num);
